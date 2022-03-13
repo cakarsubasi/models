2658f5227152553468f3066354a37a8dbb7de966
Cem Akarsubasi <cakarsubasi18@ku.edu.tr>
2022-03-13 15:30:11 +0300

    updated tfrecord creation scripts

--- PrepareMuscima/PrepareMuscimaFullPage.ps1
+++ PrepareMuscima/PrepareMuscimaFullPage.ps1
@@ -5,3 +5,9 @@ python dataset_splitter.py --source_directory=data/muscima_pp/v2.0/data/images -
 python create_muscima_tf_record.py --data_dir=data/training_validation_test --set=training --annotations_dir=Full_Page_Annotations --output_path=data/all_classes_writer_independent_split/training.record --label_map_path=mapping_all_classes.txt
 python create_muscima_tf_record.py --data_dir=data/training_validation_test --set=validation --annotations_dir=Full_Page_Annotations --output_path=data/all_classes_writer_independent_split/validation.record --label_map_path=mapping_all_classes.txt
 python create_muscima_tf_record.py --data_dir=data/training_validation_test --set=test --annotations_dir=Full_Page_Annotations --output_path=data/all_classes_writer_independent_split/test.record --label_map_path=mapping_all_classes.txt
+
+Copy-Item mapping_all_classes.txt ..
+
+Move-Item ./data/all_classes_writer_independent_split/validation.record ../data/muscima/validation.record -Force
+Move-Item ./data/all_classes_writer_independent_split/training.record ../data/muscima/training.record -Force
+Move-Item ./data/all_classes_writer_independent_split/test.record ../data/muscima/test.record -Force
